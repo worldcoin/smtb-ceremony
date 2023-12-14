dfa85ae779157d48d52fa38957045c3c6a92b81d
dcbuilder <dcbuilder@protonmail.com>
2023-12-15 00:09:29 +0100

    fix binary pattern matching in contribution script

--- contribute.sh
+++ contribute.sh
@@ -48,15 +48,15 @@ echo "Contributing OS randomness to the ceremony..."
 
 echo "Contributing to insertion circuits trusted setup (will take a long time, variable on hardware specs)..."
 
-./semaphore-mtb-setup p2c insertion_b10t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b10t30c$CONTRIBUTION_NUMBER.ph2
-./semaphore-mtb-setup p2c insertion_b100t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b100t30c$CONTRIBUTION_NUMBER.ph2
-./semaphore-mtb-setup p2c insertion_b600t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b600t30c$CONTRIBUTION_NUMBER.ph2
-./semaphore-mtb-setup p2c insertion_b1200t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b1200t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c insertion_b10t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b10t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c insertion_b100t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b100t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c insertion_b600t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b600t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c insertion_b1200t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 insertion_b1200t30c$CONTRIBUTION_NUMBER.ph2
 
 echo "Contributing to deletion circuits trusted setup (should take less, only two small circuits)..."
 
-./semaphore-mtb-setup p2c deletion_b10t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 deletion_b10t30c$CONTRIBUTION_NUMBER.ph2
-./semaphore-mtb-setup p2c deletion_b100t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 deletion_b100t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c deletion_b10t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 deletion_b10t30c$CONTRIBUTION_NUMBER.ph2
+./semaphore-mtb-setup* p2c deletion_b100t30c$PREVIOUS_CONTRIBUTION_NUMBER.ph2 deletion_b100t30c$CONTRIBUTION_NUMBER.ph2
 
 split -n 2  "insertion_b1200t30c$CONTRIBUTION_NUMBER.ph2" "insertion_b1200t30c$CONTRIBUTION_NUMBER.ph2_parts_"
 
