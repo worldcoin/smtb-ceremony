00c5b7b9c7531f4e43fd8e1c8c2874db378da69c
dcbuilder <dcbuilder@protonmail.com>
2023-12-22 12:53:25 +0100

    update verification script

--- contribution_verification/verify.sh
+++ contribution_verification/verify.sh
@@ -17,25 +17,25 @@ fi
 
 # download last contribution files for all six circuits
 echo "Downloading last contribution files for all six circuits..."
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b10/deletion_b10t30c16.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b100/deletion_b100t30c16.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b10/insertion_b10t30c16.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b100/insertion_b100t30c16.ph2
-#wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b600/insertion_b600t30c16.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c16.ph2_parts_aa
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c16.ph2_parts_ab
+curl --output deletion_b10t30c16.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b10/deletion_b10t30c16.ph2
+curl --output deletion_b100t30c16.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b100/deletion_b100t30c16.ph2
+curl --output insertion_b10t30c16.ph2  https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b10/insertion_b10t30c16.ph2
+curl --output insertion_b100t30c16.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b100/insertion_b100t30c16.ph2
+curl --output insertion_b600t30c16.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b600/insertion_b600t30c16.ph2
+curl --output insertion_b1200t30c16.ph2_parts_aa https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c16.ph2_parts_aa
+curl --output insertion_b1200t30c16.ph2_parts_ab https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c16.ph2_parts_ab
 
 cat insertion_b1200t30c16.ph2_parts_aa insertion_b1200t30c16.ph2_parts_ab > insertion_b1200t30c16.ph2
 
 # download zeroeth contribution files for all six circuits
 echo "Downloading zeroeth contribution files for all six circuits..."
 
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b10/deletion_b10t30c0.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b100/deletion_b100t30c0.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b10/insertion_b10t30c0.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b100/insertion_b100t30c0.ph2
-#wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b600/insertion_b600t30c0.ph2
-wget https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c0.ph2
+curl --output deletion_b10t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b10/deletion_b10t30c0.ph2
+curl --output deletion_b100t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/deletion_b100/deletion_b100t30c0.ph2
+curl --output insertion_b10t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b10/insertion_b10t30c0.ph2
+curl --output insertion_b100t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b100/insertion_b100t30c0.ph2
+curl --output insertion_b600t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b600/insertion_b600t30c0.ph2
+curl --output insertion_b1200t30c0.ph2 https://semaphore-mtb-trusted-setup-ceremony.s3.amazonaws.com/insertion_b1200/insertion_b1200t30c0.ph2
 
 # verify the integrity of the contributions
 echo "Verifying integrity of the contributions..."
@@ -45,7 +45,7 @@ cd semaphore-mtb-setup
 ./semaphore-mtb-setup* p2v ../deletion_b100t30c16.ph2 ../deletion_b100t30c0.ph2
 ./semaphore-mtb-setup* p2v ../insertion_b10t30c16.ph2 ../insertion_b10t30c0.ph2
 ./semaphore-mtb-setup* p2v ../insertion_b100t30c16.ph2 ../insertion_b100t30c0.ph2
-#./semaphore-mtb-setup* p2v ../insertion_b600t30c16.ph2 ../insertion_b600t30c0.ph2
+./semaphore-mtb-setup* p2v ../insertion_b600t30c16.ph2 ../insertion_b600t30c0.ph2
 ./semaphore-mtb-setup* p2v ../insertion_b1200t30c16.ph2 ../insertion_b1200t30c0.ph2
 
 echo "You can check that the outputted hashes match the ones located in the text files in the contribution_verification folder."
@@ -63,7 +63,7 @@ mv deletion_b10t30c16.ph2 deletion_b10
 mv deletion_b100t30c16.ph2 deletion_b100
 mv insertion_b10t30c16.ph2 insertion_b10
 mv insertion_b100t30c16.ph2 insertion_b100
-#mv insertion_b600t30c16.ph2 insertion_b600
+mv insertion_b600t30c16.ph2 insertion_b600
 mv insertion_b1200t30c16.ph2 insertion_b1200
 
 # Check if the gnark-contract-generator folder exists
@@ -92,10 +92,10 @@ cd insertion_b100
 ../semaphore-mtb-setup/semaphore-mtb-setup* key insertion_b100t30c16.ph2
 ../gnark-contract-generator/gnark-contract-generator* ps-vk --vk vk --out insertion_b100t30_verifier.sol
 cd ..
-#cd insertion_b600
-#../semaphore-mtb-setup/semaphore-mtb-setup* key insertion_b600t30c16.ph2
-#../gnark-contract-generator/gnark-contract-generator* ps-vk --vk vk --out insertion_b600t30_verifier.sol
-#cd ..
+cd insertion_b600
+../semaphore-mtb-setup/semaphore-mtb-setup* key insertion_b600t30c16.ph2
+../gnark-contract-generator/gnark-contract-generator* ps-vk --vk vk --out insertion_b600t30_verifier.sol
+cd ..
 cd insertion_b1200
 ../semaphore-mtb-setup/semaphore-mtb-setup* key insertion_b1200t30c16.
 ../gnark-contract-generator/gnark-contract-generator* ps-vk --vk vk --out insertion_b1200t30_verifier.sol
